f3b730347f425dfe9c34391e044024ead95bc216
Vikas Swain <vikas@Vikass-MacBook-Air.local>
2024-05-31 00:22:30 +0530

    checkpoint till the customer form design with alert dialog is done

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,13 @@
+import { CalendarForm } from "@/components/CalendarForm";
 import CustButton from "@/components/custom-components/CustButton";
+import { Calendar } from "@/components/ui/calendar";
 
 export default function () {
     return (
         <>
             <CustButton label="hello"/>
-            hi there
+            <Calendar />
+            <CalendarForm/>
         </>
     )
 }

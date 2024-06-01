96ba0c5be41d3a5456a375cfde6ab6cab4039c5d
Vikas Swain <vikas@Vikass-MacBook-Air.local>
2024-06-01 17:35:54 +0530

    updated commit for push

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDu8OSnOSEk6-hVdsGg8puP7sGlrCj6M4M",
-  authDomain: "ferrera-99389.firebaseapp.com",
-  projectId: "ferrera-99389",
-  storageBucket: "ferrera-99389.appspot.com",
-  messagingSenderId: "266395950332",
-  appId: "1:266395950332:web:63ee090c9a3e6f4d614b79",
-  measurementId: "G-Z7D0H4PXT4"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
